46e839c66859c705aae59b6968bef2946b716c5d
mar <maristein3@gmail.com>
2017-09-26 12:19:09 +0200

    Implement TaskCategory class

--- src/app/task-category.ts
+++ src/app/task-category.ts
@@ -1,3 +1,13 @@
+import { User } from "./user";
+import { Task } from "./task";
+
 export class TaskCategory {
-    
+    TaskCategoryId: number;
+    Name: string = '';
+    User: User;
+    Tasks: Array<Task>;
+
+    constructor(values: Object = {}) {
+        Object.assign(this, values);
+      }
 }
