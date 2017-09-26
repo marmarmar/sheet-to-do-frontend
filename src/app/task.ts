ed2c0ec1b089ccc6d4d86d7651c596b29481f237
mar <maristein3@gmail.com>
2017-09-26 12:15:53 +0200

    Add import statement to task.ts

--- src/app/task.ts
+++ src/app/task.ts
@@ -1,4 +1,5 @@
 import { User } from "./user";
+import { TaskCategory } from "./task-category";
 
 export class Task {
     TaskId: number;
@@ -7,7 +8,7 @@ export class Task {
     Description: string = '';
     Done: boolean = false;
     User: User;
-    // TaskCategory: TaskCategory;
+    TaskCategory: TaskCategory;
     IsArchived: boolean = false;
   
     constructor(values: Object = {}) {
