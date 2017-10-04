b4a916b29d92283fa026b9bb111e95ac9ef4e250
Diana Tokarska <tokarskadiana@gmail.com>
2017-10-04 14:00:22 +0200

    Refactor AppComponent by creating TasksComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import {TaskDataService} from './services/task-data.service';
-import { Task } from './models/task';
 
 @Component({
 
@@ -11,76 +9,9 @@ import { Task } from './models/task';
 })
 export class AppComponent implements OnInit{
 
-  tasks: Task[] = [];
-  newTask: Task = new Task();
-
   public ngOnInit() {
-   this.getAllTasks();
   }
 
-  constructor(private taskDataService: TaskDataService) {
+  constructor() {
   }
-
-  getAllTasks() {
-    this.taskDataService
-    .getAllTasks()
-    .subscribe(
-      (tasks) => {
-        this.tasks = tasks;
-      }
-    );
-  }
-  onToggleTaskDone(task) {
-    this.taskDataService.
-    toggleTaskDone(task).
-    subscribe(
-      (updatedTask) => {
-        task = updatedTask;
-      }
-    );
-  }
-
-  onAddTask(task: Task) {
-    this.taskDataService
-    .addTask(task)
-    .subscribe(
-      (newTask) => {
-        this.tasks = this.tasks.concat(newTask);
-      }
-    );
-  }
-
-  onRemoveTask(task) {
-    this.taskDataService.
-    deleteTaskById(task.taskId)
-    .subscribe(
-      (_) => {
-        this.tasks = this.tasks.filter((t) => t.taskId !== task.taskId);
-      }
-    );
-  }
-
-  onUpdateTask(task) {
-    this.taskDataService.
-    updateTask(task).
-    subscribe(
-      (updatedTask) => {
-        task = updatedTask;
-      }
-    );
-    console.log('hello');
-  }
-
-  onArchiveTask(task) {
-      this.taskDataService
-          .archiveTask(task)
-          .subscribe(
-          (archiveTask) => {
-              task = archiveTask;
-              this.getAllTasks();
-          }
-      );
-
-  }
-
 }
