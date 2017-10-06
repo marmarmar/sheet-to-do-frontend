38eb154cf7d046b2ec5c43b33abff449fcb44608
monika.plocica <monika.plocica@gmail.com>
2017-10-06 22:52:22 +0200

    Create event emitter to create new TaskCategory

--- src/app/components/task-category-footer/task-category-footer.component.ts
+++ src/app/components/task-category-footer/task-category-footer.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {TaskCategory} from '../../models/task-category';
 
 @Component({
   selector: 'app-task-category-footer',
   templateUrl: './task-category-footer.component.html',
   styleUrls: ['./task-category-footer.component.css']
 })
-export class TaskCategoryFooterComponent implements OnInit {
+export class TaskCategoryFooterComponent implements OnInit{
+  newTaskCategory: TaskCategory = new TaskCategory();
+
+  @Output()
+  add: EventEmitter<TaskCategory> = new EventEmitter();
 
   constructor() { }
-
+  addTaskCategory() {
+    this.add.emit(this.newTaskCategory);
+    this.newTaskCategory = new TaskCategory();
+  }
   ngOnInit() {
   }
-
 }
