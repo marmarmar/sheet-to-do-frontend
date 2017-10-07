5c6c3d7b2abb084b325e583c32ab51ed61ba3b33
Stefan Kania <stefankania95@gmail.com>
2017-10-07 17:24:26 +0200

    Refactor task-data service to handle all task-categories

--- src/app/services/task-data.service.ts
+++ src/app/services/task-data.service.ts
@@ -4,27 +4,49 @@ import { ApiService } from './api.service';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
 import { Http, Response } from '@angular/http';
-import {TaskCategory} from '../models/task-category';
+import {TaskCategoryCustom} from '../models/task-category-custom';
+import {TaskCategoryDate} from '../models/task-category-date';
+import {TaskCategory} from '../interfaces/task-category';
 
 const API_URL = environment.apiUrl;
 
 @Injectable()
 export class TaskDataService extends ApiService {
 
+  getTasksByCategory(taskCategory: TaskCategory): Observable<Task[]> {
+    switch (taskCategory.getType()) {
+      case 'TaskCategoryAll': {
+        return this.getAllTasks();
+      }
+      case 'TaskCategoryDate': {
+        return this.getTasksByDateCategory(<TaskCategoryDate>taskCategory);
+      }
+      case 'TaskCategoryCustom': {
+        return this.getTasksByCustomCategory(<TaskCategoryCustom>taskCategory);
+      }
+    }
+  }
 
   // API: GET /tasks
-  getAllTasks(): Observable<Task[]> {
-    return this.http.get(API_URL + 'tasks?userId=1')
-    .map(response => {
-      const tasks = response.json();
-      return tasks.map((task) => new Task(task));
-    })
-    .catch(this.handleError);
+  private getAllTasks(): Observable<Task[]> {
+    return this.http.get(`${API_URL}tasks?userId=1`)
+      .map(response => {
+        const tasks = response.json();
+        return tasks.map((task) => new Task(task));
+      })
+      .catch(this.handleError);
   }
 
   // API: GET /tasks?taskCategoryId=
-  getTasksByCustomCategory(taskCategory: TaskCategory): Observable<Task[]> {
-    return this.http.get(API_URL + `tasks?taskCategoryId=${taskCategory.taskCategoryId}`)
+  private getTasksByCustomCategory(taskCategory: TaskCategoryCustom): Observable<Task[]> {
+    return this.http.get(`${API_URL}tasks?taskCategoryId=${taskCategory.taskCategoryId}`)
+      .map(response => response.json() as Task[])
+      .catch(this.handleError);
+  }
+
+  // API: GET /tasks?userId= &startDate= &endDate=
+  private getTasksByDateCategory(taskCategory: TaskCategoryDate): Observable<Task[]> {
+    return this.http.get(`${API_URL}tasks?userId=1&startDate=${taskCategory.startDate}&endDate=${taskCategory.endDate}`)
       .map(response => response.json() as Task[])
       .catch(this.handleError);
   }
@@ -60,8 +82,8 @@ export class TaskDataService extends ApiService {
   }
 
   // API: PATCH /tasks/:id
-  toggleTaskDone(task: Task){
-    task.done=!task.done;
+  toggleTaskDone(task: Task) {
+    task.done = !task.done;
     return this.http
     .patch(API_URL + 'tasks/' + task.taskId, [{ op: 'replace', path: 'Done', value: task.done }])
     .map(response => null)
