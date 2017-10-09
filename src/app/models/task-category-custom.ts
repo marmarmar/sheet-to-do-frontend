a4d84ccd0ee500709a29ecd9d5ce1374972fa543
Stefan Kania <stefankania95@gmail.com>
2017-10-09 11:58:19 +0200

    Remove unecessary fields from task-category-custom model

--- src/app/models/task-category-custom.ts
+++ src/app/models/task-category-custom.ts
@@ -3,8 +3,6 @@ import {TaskCategory} from '../interfaces/task-category';
 export class TaskCategoryCustom implements TaskCategory {
     taskCategoryId: number;
     name: string;
-    user: any;
-    tasks: any;
 
   constructor(values: Object = {}) {
     Object.assign(this, values);
